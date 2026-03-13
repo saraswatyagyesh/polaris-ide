a135e32fd489a99ddf10764c7d7f2f469e928841
Yagyesh <saraswatthrust@gmail.com>
2026-03-13 18:23:45 +0530

    feat: Finish authentication by integrating Clerk authentication and update layout with user sign-in options

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { IBM_Plex_Mono, Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
+import { ClerkProvider, SignInButton, SignUpButton, SignedIn, SignedOut, UserButton } from '@clerk/nextjs'
+import { dark } from "@clerk/themes";
 
 const inter = Inter({
   variable: "--font-inter",
@@ -25,14 +27,32 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
-      <body
-        className={`${inter.variable} ${plexMono.variable} antialiased`}
-      >
-        <ThemeProvider attribute="class" defaultTheme="dark" enableSystem disableTransitionOnChange >
-          {children} 
-        </ThemeProvider>
-      </body>
-    </html>
+    <ClerkProvider appearance={{ theme: dark, }}>
+      <html lang="en" suppressHydrationWarning>
+        <body
+          className={`${inter.variable} ${plexMono.variable} antialiased`}
+        >
+          <ThemeProvider attribute="class" defaultTheme="dark" enableSystem disableTransitionOnChange >
+            
+            <header>
+                <SignedOut>
+                <SignInButton />
+                <SignUpButton>
+                  <button className="bg-rose-500 text-white p-2 rounded">
+                    Sign Up
+                  </button>
+                </SignUpButton>
+              </SignedOut>
+
+              <SignedIn>
+                <UserButton />
+              </SignedIn>
+            </header>
+
+            {children} 
+          </ThemeProvider>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
