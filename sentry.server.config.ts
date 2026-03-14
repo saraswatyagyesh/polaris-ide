4e8b42499c00e4e395d43024ee35a4fabe93386e
Yagyesh <saraswatthrust@gmail.com>
2026-03-15 02:49:27 +0530

    feat: Integrate Sentry for error monitoring

---
+++ sentry.server.config.ts
@@ -0,0 +1,19 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://3bd03c867ce8b27e4c8caef61d80c467@o4510929957879808.ingest.us.sentry.io/4511044857757696",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Enable logs to be sent to Sentry
+  enableLogs: true,
+
+  // Enable sending user PII (Personally Identifiable Information)
+  // https://docs.sentry.io/platforms/javascript/guides/nextjs/configuration/options/#sendDefaultPii
+  sendDefaultPii: true,
+});
--- src/inngest/client.ts
+++ src/inngest/client.ts
@@ -1,3 +1,7 @@
 import { Inngest } from "inngest";
+import { sentryMiddleware } from "@inngest/middleware-sentry";
 
-export const inngest = new Inngest({ id: "Polaris IDE" });
+export const inngest = new Inngest({
+    id: "Polaris IDE",
+    midddleware: [sentryMiddleware()],
+});
