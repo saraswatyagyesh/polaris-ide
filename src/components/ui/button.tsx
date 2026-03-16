5be18a89b2904f8699db576bc36829c6baecc740
Yagyesh <saraswatthrust@gmail.com>
2026-03-17 02:04:53 +0530

    feat: Implement file explorer with Convex backend for project file management.

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -19,6 +19,7 @@ const buttonVariants = cva(
         ghost:
           "hover:bg-accent hover:text-accent-foreground dark:hover:bg-accent/50",
         link: "text-primary underline-offset-4 hover:underline",
+        highlight: "bg-transparent hover:bg-accent-foreground/5",
       },
       size: {
         default: "h-9 px-4 py-2 has-[>svg]:px-3",
@@ -26,9 +27,10 @@ const buttonVariants = cva(
         sm: "h-8 gap-1.5 rounded-md px-3 has-[>svg]:px-2.5",
         lg: "h-10 rounded-md px-6 has-[>svg]:px-4",
         icon: "size-9",
-        "icon-xs": "size-6 rounded-md [&_svg:not([class*='size-'])]:size-3",
+        // "icon-xs": "size-6 rounded-md [&_svg:not([class*='size-'])]:size-3",
         "icon-sm": "size-8",
         "icon-lg": "size-10",
+        "icon-xs": "size-5.5 rounded",
       },
     },
     defaultVariants: {
--- src/features/projects/components/project-id-view.tsx
+++ src/features/projects/components/project-id-view.tsx
@@ -4,6 +4,13 @@ import { cn } from "@/lib/utils";
 import { Id } from "../../../../convex/_generated/dataModel";
 import { useState } from "react";
 import { FaGithub } from "react-icons/fa";
+import { Allotment } from "allotment";
+import { FileExplorer } from "./file-explorer";
+
+const MIN_SIDEBAR_WIDTH = 200;
+const MAX_SIDEBAR_WIDTH = 800;
+const DEFAULT_SIDEBAR_WIDTH = 350;
+const DEFAULT_MAIN_SIZE = 1000;
 
 const Tab = ({ label, isActive, onClick }: { label: string, isActive: boolean, onClick: () => void; }) => {
 
@@ -36,7 +43,14 @@ export const ProjectIdView = ({ projectId }: { projectId: Id<"projects"> }) => {
             <div className="flex-1 relative">
 
                 <div className={cn("absolute inset-0", activeView === "editor" ? "visible" : "invisible")}>
-                    <div>Editor</div>
+                    <Allotment defaultSizes={[DEFAULT_SIDEBAR_WIDTH, DEFAULT_MAIN_SIZE]}>
+                        <Allotment.Pane snap minSize={MIN_SIDEBAR_WIDTH} maxSize={MAX_SIDEBAR_WIDTH} preferredSize={DEFAULT_SIDEBAR_WIDTH}>
+                            <FileExplorer projectId={projectId} />
+                        </Allotment.Pane>
+                        <Allotment.Pane>
+                            <p>Editor view</p>
+                        </Allotment.Pane>
+                    </Allotment>
                 </div>
 
                 <div className={cn("absolute inset-0", activeView === "preview" ? "visible" : "invisible")}>
